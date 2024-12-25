890bed5e55dd9728b6865dd4bd91f52032b9257d
Jhon8aC <tkg.jhon123@gmail.com>
2024-12-25 12:44:44 -0500

    Project added

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : base("Han ocurrido uno o más errores")
+        {
+            Errors = new List<string>();
+
+        }
+        public List<string> Errors { get; }
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ Domain/Entities/SuperHeroe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class SuperHeroe : AuditableBaseEntity
+    {
+        public string SuperHeroeName { get; set; }
+        public string LegalName { get; set; }   
+        public int Age { get; set; }
+        public string SuperPower { get; set; }
+        public string Gender { get; set; }
+        public string Editorial { get; set; }
+
+
+        public DateTime 
+    }
+}
